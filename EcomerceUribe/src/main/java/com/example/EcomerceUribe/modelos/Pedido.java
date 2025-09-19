a5c3f4f9897e7d7a540aba0cd9d801144a67cec7
Jessica Mora <yesik1896@hotmail.com>
2025-09-18 20:21:27 -0500

    se realiza la relacion 1 a m entre cliente y pedido

--- EcomerceUribe/src/main/java/com/example/EcomerceUribe/modelos/Pedido.java
+++ EcomerceUribe/src/main/java/com/example/EcomerceUribe/modelos/Pedido.java
@@ -1,8 +1,10 @@
 package com.example.EcomerceUribe.modelos;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -12,20 +14,33 @@ public class Pedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    //Integer montoTotal
-    //LocalDate fechaCreacion
-    //LocalDate fechaEntrega
-    //Integer costoEnvio
+    private Integer montoTotal;
+    private LocalDate fechaCreacion;
+    private LocalDate fechaEntrega;
+    private Integer costoEnvio;
+
 
     @OneToMany(mappedBy = "pedido")
     @JsonManagedReference(value = "relacionpedidoproducto")
     private List<Producto> productos;
 
+
+    @ManyToOne
+    @JoinColumn(name = "fk_cliente", referencedColumnName = "id")
+    @JsonBackReference(value = "relacionclientepedidolist")
+    private Cliente cliente;
+
+
+
     public Pedido() {
     }
 
-    public Pedido(Integer id) {
+    public Pedido(Integer id, Integer montoTotal, LocalDate fechaCreacion, LocalDate fechaEntrega, Integer costoEnvio) {
         this.id = id;
+        this.montoTotal = montoTotal;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaEntrega = fechaEntrega;
+        this.costoEnvio = costoEnvio;
     }
 
     public Integer getId() {
@@ -34,5 +49,37 @@ public class Pedido {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Integer getMontoTotal() {
+        return montoTotal;
+    }
+
+    public void setMontoTotal(Integer montoTotal) {
+        this.montoTotal = montoTotal;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public LocalDate getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(LocalDate fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public Integer getCostoEnvio() {
+        return costoEnvio;
+    }
+
+    public void setCostoEnvio(Integer costoEnvio) {
+        this.costoEnvio = costoEnvio;
     }
 }
