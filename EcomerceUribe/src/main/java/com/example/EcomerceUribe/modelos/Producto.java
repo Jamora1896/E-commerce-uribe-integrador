9022c6b4a50dd269a8e29dc1196b192173615f9b
Jessica Mora <yesik1896@hotmail.com>
2025-10-02 21:14:35 -0500

    se crean repositorios faltantes

--- EcomerceUribe/src/main/java/com/example/EcomerceUribe/modelos/Producto.java
+++ EcomerceUribe/src/main/java/com/example/EcomerceUribe/modelos/Producto.java
@@ -1,5 +1,6 @@
 package com.example.EcomerceUribe.modelos;
 
+import com.example.EcomerceUribe.ayudas.Categoria;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
@@ -10,13 +11,13 @@ public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    //String nombre
-    //String fotografia
-    //String descripcion
-    //ENUM categoria(Camiseta, Pantalon, vestido, Interior, Zapato, Chaqueta,Polo,Bermuda, Accesorio)
-    //Integer precioUnitario
-    //String marca
-    //boolean aplicaDescuento
+    private String nombre;
+    private String fotografia;
+    private String descripcion;
+    private Categoria categoria;
+    private Integer precioUnitario;
+    private String marca;
+    private boolean aplicaDescuento;
     @ManyToOne
     @JoinColumn(name = "fk_pedido", referencedColumnName = "id")
     @JsonBackReference(value = "relacionpedidoproducto")
@@ -25,8 +26,16 @@ public class Producto {
     public Producto() {
     }
 
-    public Producto(Integer id) {
+    public Producto(Integer id, String nombre, String fotografia, String descripcion, Categoria categoria, Integer precioUnitario, String marca, boolean aplicaDescuento, Pedido pedido) {
         this.id = id;
+        this.nombre = nombre;
+        this.fotografia = fotografia;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.precioUnitario = precioUnitario;
+        this.marca = marca;
+        this.aplicaDescuento = aplicaDescuento;
+        this.pedido = pedido;
     }
 
     public Integer getId() {
@@ -35,5 +44,69 @@ public class Producto {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFotografia() {
+        return fotografia;
+    }
+
+    public void setFotografia(String fotografia) {
+        this.fotografia = fotografia;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(Integer precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public boolean isAplicaDescuento() {
+        return aplicaDescuento;
+    }
+
+    public void setAplicaDescuento(boolean aplicaDescuento) {
+        this.aplicaDescuento = aplicaDescuento;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
     }
 }
